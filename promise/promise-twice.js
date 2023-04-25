f1383dab3480a4a4a9a7ffe75bfd835860a30df4
zouweizhe <zouweizhe@guoxintianyu.com>
2023-04-25 13:48:58 +0800

    尝试手写下promise

--- promise/promise-twice.js
+++ promise/promise-twice.js
@@ -38,7 +38,7 @@ class Promise {
 
         const promise2 = new Promise((resolve, reject) => {
             onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
-            onReject = typeof onReject === 'function' ? onReject : e => throw e
+            onReject = typeof onReject === 'function' ? onReject : e => {throw e;}
 
             if (this.resultStatus === Promise.rejected) {
                 setTimeout(() => {
@@ -95,4 +95,47 @@ function handlePromise(promise2, x, resolve, reject) {
     if (promise2 === x) {
         return new Error('同一个promise')
     }
+    let hasCalled = false
+    // 判断是否是一个promise
+    if (typeof x === 'object' && x !== null || typeof x === 'function') {
+        // 可能是一个promise
+        // 判断是否含有then方法
+        try {
+            const then = x.then
+            if (typeof then === 'function') {
+                then.call(
+                    this,
+                    y => {
+                        if(hasCalled)return
+                        hasCalled = true
+                        handlePromise(promise2, y, resolve, reject)
+                    },
+                    r => {
+                        if(hasCalled)return
+                        hasCalled = true
+                        reject(r)
+                    }
+                )
+            } else {
+                resolve(x)
+            }
+        } catch (e) {
+            if(hasCalled)return
+            hasCalled = true
+            reject(e)
+        }
+    } else {
+        resolve(x)
+    }
 }
+module.exports = Promise;
+
+Promise.deferred = function () {
+    // deferred  all race catch ...
+    let dfd = {};
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+};
